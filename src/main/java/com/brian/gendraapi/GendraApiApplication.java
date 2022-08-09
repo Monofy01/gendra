569fe923770347c9212fa6eac5e1538375e7ba48
monofy01 <bri_riva@hotmail.com>
2022-08-09 14:49:49 -0500

    [INIT] init repo

---
+++ src/main/java/com/brian/gendraapi/GendraApiApplication.java
@@ -0,0 +1,13 @@
+package com.brian.gendraapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GendraApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GendraApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/brian/gendraapi/GendraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brian.gendraapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GendraApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
